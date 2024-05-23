a42ef5b83dfd762566e1f47aebc6c8089a063451
preDeploy <168213155+preDeploy@users.noreply.github.com>
2024-05-22 20:13:14 -0700

    Add files via upload

--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request, HTTPException, Depends, File, UploadFile, Form
+from fastapi import Body, FastAPI, Request, HTTPException, Depends, File, UploadFile, Form
 from typing import Optional, List
 from PIL import Image
 import io
@@ -10,55 +10,106 @@ from datetime import datetime
 import os
 import time
 import asyncio
-import csv
-from sqlalchemy import create_engine, Column, String
+from sqlalchemy import MetaData, create_engine, Column, String, Table, inspect, text
 from sqlalchemy.orm import sessionmaker, Session, declarative_base
-# from sqlalchemy.ext.declarative import
-# from models import User
 import stripe
-import cv2
 import boto3
+import re
+import string
+from pathlib import Path
 
 Base = declarative_base()
 
 
-
-
 class S3():
     def __init__(self):
-        self.AWS_ACCESS_ID=""
-        self.AWS_SECRET_KEY=""
-        self.s3 = boto3.client('s3', aws_access_key_id=self.AWS_ACCESS_ID, aws_secret_access_key=self.AWS_SECRET_KEY)
+        self.AWS_ACCESS_ID = ""
+        self.AWS_SECRET_KEY = ""
+        self.s3 = boto3.client('s3', aws_access_key_id=self.AWS_ACCESS_ID,
+                               aws_secret_access_key=self.AWS_SECRET_KEY)
         self.bucket_name = "doloreschatbucket"
+        self.session = boto3.Session(
+            aws_access_key_id=self.AWS_ACCESS_ID, aws_secret_access_key=self.AWS_SECRET_KEY)
+        self.bucket_s3 = self.session.resource('s3')
+        self.bucket = self.bucket_s3.Bucket(self.bucket_name)
 
     def resizeThumbnail(self, image, resize):
         min_dimension = min(image.width, image.height)
         resize_factor = resize[0] / min_dimension
-        # image.thumbnail(resize)
         new_width = int(image.width * resize_factor)
         new_height = int(image.height * resize_factor)
-        resized_image = image.resize((new_width, new_height), Image.Resampling.LANCZOS)
+        resized_image = image.resize(
+            (new_width, new_height), Image.Resampling.LANCZOS)
         left = (resized_image.width - resize[0]) / 2
         top = (resized_image.height - resize[1]) / 2
-        cropped_image = resized_image.crop((left, top, left + resize[0], top + resize[1]))
+        cropped_image = resized_image.crop(
+            (left, top, left + resize[0], top + resize[1]))
         return cropped_image
-    
-    def upload_newPic(self, file, directory):        
+
+    def upload_newPic(self, file, directory):
+        print('uploading!')
         profilePic_dir = f"{directory}/profilePic/"
         self.s3.put_object(
             Bucket=self.bucket_name,
             Key=f"{profilePic_dir}user.png",
             Body=file,
-            # ExtraArgs={'ACL': 'public-read'}
-            )
-        
+        )
+        print('uploaded')
+
     def upload_files(self, file_path, file_dir):
         self.s3.upload_file(
             file_path,
             self.bucket_name,
             file_dir,
-            # ExtraArgs={'ACL': 'public-read'}
         )
+
+    def getLegalName(self, filename):
+        legal_chars = set(string.ascii_letters + string.digits + '._-')
+        cleaned_filename = ''.join(c for c in filename if c in legal_chars)
+        cleaned_filename = re.sub(r'\.+', '.', cleaned_filename)
+        cleaned_filename = cleaned_filename.strip('. ')
+        if not cleaned_filename:
+            cleaned_filename = 'file'
+        max_length = 255
+        if len(cleaned_filename) > max_length:
+            cleaned_filename = cleaned_filename[:max_length]
+        return cleaned_filename
+
+    def s3_headObj(self, s3_key):
+        try:
+            self.s3.head_object(Bucket=self.bucket_name, Key=s3_key)
+            return True
+        except:
+            return False
+
+    def checkfile(self, destination_dir, filename):
+        counter = 1
+        s3_key = f"{destination_dir}/{filename}"
+        while self.s3_headObj(s3_key):
+            stem, ext = os.path.splitext(filename)
+            mod_filename = f"{stem}_{counter}{ext}"
+            counter += 1
+            s3_key = f"{destination_dir}/{mod_filename}"
+        return s3_key
+
+    def renameDir(self, oldname, newname):
+        for obj in self.bucket.objects.filter(Prefix=oldname):
+            old_source = {'Bucket': self.bucket_name, 'Key': obj.key}
+            new_key = obj.key.replace(oldname, newname, 1)
+            new_obj = self.bucket.Object(new_key)
+            new_obj.copy(old_source)
+
+        for obj in self.bucket.objects.filter(Prefix=oldname):
+            self.bucket_s3.Object(self.bucket_name, obj.key).delete()
+        # return newname
+
+    def createDir(self, username):
+        newFolders = [f'users/{username}/profilePic/',
+                      f'users/{username}/files/', f'users/{username}/forms/']
+        for folder in newFolders:
+            self.bucket.put_object(Key=folder, Body=b'')
+        # return username
+
 
 class User(Base):
     __tablename__ = 'users'
@@ -109,13 +160,13 @@ class Dolores():
                     "content": f"This is the previous context:\n\nuser:{self.context['user']}\nassistant:{self.context['assistant']}. And this is the current prompt: {user_input}. Respond in plain string format, no special formatting",
                 }
             ]
-        )        
+        )
+
         run = client.beta.threads.runs.create(
             thread_id=thread.id,
             assistant_id=self.OPENAI_ASSISTANT_ID
         )
         while run.status != "completed":
-            # print(f"counter {counter}: {run.status}")
             run = client.beta.threads.runs.retrieve(
                 thread_id=thread.id,
                 run_id=run.id
@@ -189,7 +240,6 @@ class Database():
             db.close()
 
     def add_user(self, details, db):
-        # print(details.keys())
         if details['user_location'] != '':
             location = details['user_location']
         else:
@@ -199,12 +249,30 @@ class Database():
         else:
             password = None
 
-        print(password)
+        inspector = inspect(self.engine)
+        if not inspector.has_table(details['username']):
+            metadata = MetaData()
+            new_table = Table(
+                details['username'],
+                metadata,
+                Column('user_messages', String),
+                Column('bot_messages', String),
+                Column('files', String),
+                Column('forms', String)
+            )
+            metadata.create_all(self.engine)
         new_user = User(first_name=details['firstName'], last_name=details['lastName'], email=details['email'],
                         username=details['username'], profile_pic=details['profilePic'], password=password, location=location)
         db.add(new_user)
         db.commit()
         db.refresh(new_user)
+
+    def renameTable(self, oldname, newname):
+        inspector = inspect(self.engine)
+        if inspector.has_table(oldname):
+            rename_query = text(f"ALTER TABLE {oldname} RENAME TO {newname}")
+            with self.engine.begin() as conn:
+                conn.execute(rename_query)
 
     def update_subscription(self, email, tier, db):
         user = db.query(User).filter(User.email == email).first()
@@ -213,6 +281,10 @@ class Database():
             db.commit()
             db.refresh(user)
 
+    def add_messages(self, username, user_message, bot_message):
+        with self.engine.begin() as conn:
+            insert_vals = f"INSERT INTO {username} (user_messages, bot_messages) VALUES (:user_message, :bot_message)"
+            conn.execute(text(insert_vals), {"user_message": user_message, "bot_message": bot_message})
 
 class Stripe():
     def __init__(self) -> None:
@@ -224,10 +296,6 @@ class Stripe():
             'plus': {'monthly': 'price_1PHT38P6cNlgDSsm6Y7TXywW', 'yearly': 'price_1PHT3YP6cNlgDSsmZmLFLpsK'},
             'premium': {'monthly': 'price_1PGkXdP6cNlgDSsmedxP1cEU', 'yearly': 'price_1PGkYCP6cNlgDSsmJfN7djSg'}
         }
-        # self.priceID = {
-        #     'pro': {'monthly': 'price_1PEabFP6cNlgDSsmwo4VLXmn', 'yearly': 'price_1PEabFP6cNlgDSsmwo4VLXmn'},
-        #     'plus': {'monthly': 'price_1PEabFP6cNlgDSsmwo4VLXmn', 'yearly': 'price_1PEabFP6cNlgDSsmwo4VLXmn'}
-        # }
         self.tier = 'free'
         self.email = 'none'
         self.transaction_id = 'none'
@@ -285,6 +353,7 @@ async def get_bot_response(request: Request):
     user_message = data["user_message"]
     user = data["user"]
     bot_response = await bot_responses.chat(user_message, user)
+    sql_db.add_messages(user, user_message, bot_response)
     response = {"bot_response": bot_response}
     asyncio.create_task(bot_responses.updateContext())
     return response
@@ -295,6 +364,7 @@ async def new_user(request: Request, db: Session = Depends(sql_db.get_db)):
     data = await request.json()
     user = db.query(User).filter(User.email == data['email']).first()
     if not user:
+        s3.createDir(data['username'])
         sql_db.add_user(data, db)
 
 
@@ -303,12 +373,11 @@ async def create_checkoutSessID(request: Request):
     data = await request.json()
     coSess_ID = stripeObj.checkOutSess(
         data['tier'], data['duration'], data['email'])
-    # stripeObj.assignCID(coSess_ID)
     return {'id': coSess_ID}
 
 
 @app.post('/stripe-webhook/')
-def handle_stripeWebhook(request, db: Session = Depends(sql_db.get_db)):
+def handle_stripeWebhook(request):
     signature = request.headers.get('Stripe-Signature')
     try:
         event = stripe.Webhook.construct_event(
@@ -356,8 +425,8 @@ async def checkUser(request: Request, db: Session = Depends(sql_db.get_db)):
 @app.post('/register/')
 async def registerUser(request: Request, db: Session = Depends(sql_db.get_db)):
     data = await request.json()
-    # print(data)
     user = db.query(User).filter(User.email == data['email']).first()
+    s3.createDir(data['username'])
     if user:
         user.profile_pic = data['profilePic']
         user.first_name = data['firstName']
@@ -385,7 +454,8 @@ async def getPassword(request: Request, db: Session = Depends(sql_db.get_db)):
                     'lastName': user.last_name,
                     'email': user.email,
                     'profilePicUrl': user.profile_pic,
-                    'user_location': user.user_location}
+                    'user_location': user.location}
+
 
 @app.post('/check_username/')
 async def checkUsername(request: Request, db: Session = Depends(sql_db.get_db)):
@@ -396,11 +466,16 @@ async def checkUsername(request: Request, db: Session = Depends(sql_db.get_db)):
     else:
         return {'exists': False}
 
+
 @app.post('/update_user/')
 async def updateUser(request: Request, db: Session = Depends(sql_db.get_db)):
     data = await request.json()
-
-    user = db.query(User).filter(User.email==data['email_id']).first()
+    user = db.query(User).filter(User.email == data['email_id']).first()
+    oldname = f'users/{user.username}/'
+    newname = f"users/{data['username']}/"
+    if oldname != newname:
+        s3.renameDir(oldname, newname)
+        sql_db.renameTable(user.username, data['username'])
     user.username = data['username']
     user.first_name = data['firstName']
     user.last_name = data['lastName']
@@ -409,24 +484,23 @@ async def updateUser(request: Request, db: Session = Depends(sql_db.get_db)):
     db.commit()
     db.refresh(user)
 
+
 @app.post('/update_password/')
 async def updatePass(request: Request, db: Session = Depends(sql_db.get_db)):
     data = await request.json()
-    user = db.query(User).filter(User.email==data['email_id']).first()
+    user = db.query(User).filter(User.email == data['email_id']).first()
     user.password = data['password']
     db.commit()
     db.refresh(user)
 
+
 @app.post('/uploadProfilePic/')
 async def uploadProfilePic(profilePic: UploadFile = File(...), email_id: str = Form(...), db: Session = Depends(sql_db.get_db)):
     try:
-        file_data = await profilePic.read()    
-        user = db.query(User).filter(User.email==email_id).first()    
+        file_data = await profilePic.read()
+        user = db.query(User).filter(User.email == email_id).first()
+        print(user.username)
         user_dir = f"users/{user.username}"
-        # user_dir = 'test'
-        # profilePic_dir = f"{user_dir}/profilePic/"
-        # forms_dir = f"{user_dir}/forms/"
-        # files_dir = f"{user_dir}/files/"
         image = Image.open(io.BytesIO(file_data))
         cropped_image = s3.resizeThumbnail(image, (175, 175))
         png_data = io.BytesIO()
@@ -439,35 +513,28 @@ async def uploadProfilePic(profilePic: UploadFile = File(...), email_id: str = F
         return {'resp': False,
                 'error': str(e)}
 
-@app.post('/upload_forms/')
+
+@app.post('/upload_files/')
 async def upload_forms(files: List[UploadFile] = File(...), email_id: str = Form(...), db: Session = Depends(sql_db.get_db)):
-    user = db.query(User).filter(User.email==email_id).first()
-    user_dir = f"users/{user.username}"
+    user = db.query(User).filter(User.email == email_id).first()
+    destination_dir = f"users/{user.username}/files"
     for file in files:
-        file_extension = file.filename.split('.')[-1]
-
-        # Determine the destination directory based on the file extension
-        if file_extension.lower() in ['pdf', 'doc', 'docx', 'xls', 'xlsx']:
-            destination_dir = f"{user_dir}/forms"
-        else:
-            destination_dir = f"{user_dir}/files"
-
-        # Create a temporary file path
-        temp_file_path = f'/tmp/{file.filename}'
-
-        # Save the file to a temporary location
+        tmp_filename = s3.getLegalName(file.filename)
+        temp_file_path = Path('/tmp') / tmp_filename
+        temp_file_path.parent.mkdir(parents=True, exist_ok=True)
+        s3_key = s3.checkfile(destination_dir, tmp_filename)
         with open(temp_file_path, 'wb') as temp_file:
             content = await file.read()
             temp_file.write(content)
 
-        # Upload the file to the S3 bucket
-        s3.upload_files(temp_file_path, f"{destination_dir}/{file.filename}")
-        os.remove(temp_file_path)
+        s3.upload_files(str(temp_file_path), s3_key)
+        os.remove(str(temp_file_path))
+
 
 @app.get('/test/')
 async def test_func():
     return {'test': 'successful'}
 
 if __name__ == "__main__":
+    host = '0.0.0.0'
     uvicorn.run("backend:app", reload=True)
-    # print(f'ReLo: {bot_responses.assistant(input("You: "))}')
